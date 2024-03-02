fbc3bea7c0c25b37389bdd8cea4dac413703a93e
Daniel98bbzz <102686490+Daniel98bbzz@users.noreply.github.com>
2024-03-02 11:26:13 +0200

    Update apifunctions.js

--- CounselingSessionsClient/src/api/apifunctions.js
+++ CounselingSessionsClient/src/api/apifunctions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000/api/sessions/";
+const URL = "https://enhanced-counseling-sessions-management.onrender.com/sessions/";
 
 export const readSessions = async () => {
   try {
